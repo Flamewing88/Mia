b42913637cbf83fe50bc0262422e848a76147c91
Flamewing88 <78690207+Flamewing88@users.noreply.github.com>
2022-07-26 22:07:51 +0700

    это должен быть рабочий канвас

--- snow.js
+++ snow.js
@@ -1,13 +1,14 @@
-function init() { //переменные, инициализация
-    canvas = document.getElementById("ice"); //соединение с html
+// Инициализация переменных
+function init() {
+    canvas = document.getElementById("ice");
     canvas.width = 480; // задаём ширину холста
     canvas.height = 320; // задаём высоту холста
     context = canvas.getContext('2d');
     draw();
 }
 // Отрисовка игры
-function draw() { 
-    context.fillStyle = #a3d0c2; //это цвет прямоугольника 
-    context.fillRect(1, 1, 480px, 320px); //это типа рисует прямоугольник какой-то
+function draw() {
+    context.fillStyle = "#a3d0c2";
+    context.fillRect(0, 0, 480, 320);
 }
 init();
