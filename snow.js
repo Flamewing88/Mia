8b7c524a38c89473d828fc4a52fbe1e6253da3db
Flamewing88 <78690207+Flamewing88@users.noreply.github.com>
2022-07-27 13:07:10 +0700

    тест rect.прошлая версия рабочая.

--- snow.js
+++ snow.js
@@ -1,9 +1,18 @@
 // Инициализация переменных
 function init() {
-    canvas = document.getElementById("ice");
-    canvas.width = 480; // задаём ширину холста
-    canvas.height = 320; // задаём высоту холста
-    context = canvas.getContext('2d');
+    game = new rect("#000", 0, 0, 480, 320);
+    // Ракетки-игроки
+    ai = new rect("#fff", 10, game.height / 2 - 40, 20, 80);
+    player = new rect("#fff", game.width - 30, game.height / 2 - 40, 20, 80);
+    // количество очков
+    ai.scores = 0;
+    player.scores = 0;
+    // наш квадратный игровой "шарик"
+    ball = new rect("#fff", 40, game.height / 2 - 10, 20, 20);
+    canvas = document.getElementById("pong");
+    canvas.width = game.width;
+    canvas.height = game.height;
+    context = canvas.getContext("2d");
     draw();
 }
 // Отрисовка игры
@@ -12,3 +21,16 @@ function draw() {
     context.fillRect(1, 1, 480, 320);
 }
 init();
+
+function rect(color, x, y, width, height) {
+    this.color = color; // цвет прямоугольника
+    this.x = x; // координата х
+    this.y = y; // координата у
+    this.width = width; // ширина
+    this.height = height; // высота
+    this.draw = function() // Метод рисующий прямоугольник
+    {
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
